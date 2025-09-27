9f0a049225c8ff3b87e1284a8e2e827d1694b725
Naperality <napolytan@gmail.com>
2025-09-28 01:47:44 +0800

    add: Three Point Distance Calculator

--- ACCENTURE/DistanceCalculator.py
+++ ACCENTURE/DistanceCalculator.py
@@ -11,3 +11,19 @@
 # x3 = 3 y3 = 6
 
 # Distance is calculated as : sqrt(x2-x1)2 + (y2-y1)2
+import math
+def DistanceCalculator(x, y, z):
+    
+    x, y, z = [int(num) for num in x], [int(num) for num in y], [int(num) for num in z]
+
+    diff_one = math.sqrt(pow(y[0]-x[0],2)+pow(y[1]-x[1],2))
+    diff_two = math.sqrt(pow(z[0]-y[0],2)+pow(z[1]-y[1],2))
+    diff_three = math.sqrt(pow(z[0]-x[0],2)+pow(z[1]-x[1],2))
+
+    return f'{round(diff_one, 2)}, {round(diff_two, 2)}, and {round(diff_three, 2)}'
+    
+input_one = input("Point 1: ").split()
+input_two = input("Point 2: ").split()
+input_three = input("Point 3: ").split()
+
+print(DistanceCalculator(input_one, input_two, input_three))
