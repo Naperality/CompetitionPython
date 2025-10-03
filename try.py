7b9e6e6f6a772bf0636e042077beb385954edb1d
Naperality <napolytan@gmail.com>
2025-10-03 13:04:38 +0800

    add: factorial function to function

--- try.py
+++ try.py
@@ -1,18 +1,8 @@
-def fibonacci(n):
-    def fibo(r):
-        if r == 0 or r == 1:
-            return r
-        return fibo(r-1) + fibo(r-2)
-    return fibo(n)
-
-def factorial(n):
-    def fact(r):
+def Fact(n):
+    def factorial(r):
         if r in (0,1):
             return 1
-        return r * fact(r-1)
-    return fact(n)
+        return r * factorial(r-1)
+    return factorial(n)
 
-for i in range(10+1):
-    print(fibonacci(i), end = " ")
 
-print(factorial(5))
