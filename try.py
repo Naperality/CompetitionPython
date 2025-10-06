9debd6c02a550eccd81a1cf0df7e1e8c7163237c
Naperality <napolytan@gmail.com>
2025-10-06 15:35:34 +0800

    add: try

--- try.py
+++ try.py
@@ -1,10 +1,1 @@
-def Fact(n):
-    def factorial(r):
-        if r in (0,1):
-            return 1
-        return r * factorial(r-1)
-    return factorial(n)
 
-
-input_n = int(input("Factorial of : "))
-print(Fact(input_n))
