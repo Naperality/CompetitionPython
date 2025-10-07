5d963636c0eaa549699e8d14c48233cbc3ae2af5
Naperality <napolytan@gmail.com>
2025-10-07 16:37:30 +0800

    add: caeser cipher

--- try.py
+++ try.py
@@ -14,3 +14,16 @@ input_arr = list(map(int, input("Array: ").split()))
 target = int(input("Target: "))
 
 print(twoSum(input_arr, target))
+
+# Caeser Code
+
+def caeserCipher(word, shift):
+    result = ''
+    for letter in word:
+        base = ord('A') if letter.isupper() else ord('a')
+        result += chr((ord(letter)-base+shift) % 26 + base)
+    return result
+
+input_word = input("Enter Word: ")
+input_shift = int(input("Enter Shift: "))
+print(caeserCipher(input_word, input_shift))
