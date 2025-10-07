0d40d7c7a29e22a3924f214c6a3459f2e10deb43
Naperality <napolytan@gmail.com>
2025-10-07 16:30:03 +0800

    add: two sum solution

--- try.py
+++ try.py
@@ -1,1 +1,16 @@
+def twoSum(arr, target):
+    seen = {}
 
+    for index, num in enumerate(arr):
+        complement = target - num
+
+        if complement in seen:
+            return (index, seen[complement])
+        
+        seen[num] = index
+    return -1
+
+input_arr = list(map(int, input("Array: ").split()))
+target = int(input("Target: "))
+
+print(twoSum(input_arr, target))
