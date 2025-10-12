260a3bb56c4bfca6d90a0a70f6fdd4a796a20f7b
Naperality <napolytan@gmail.com>
2025-10-12 14:09:13 +0800

    add: Square sum

---
+++ CODEWARS/square_sum.py
@@ -0,0 +1,11 @@
+# Complete the square sum function so that it squares each number passed into it 
+# and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 
+# 1^2 + 2^2 + 2^2 = 9
+
+def square_sum(num_arr):
+    return sum(num**2 for num in num_arr)
+
+input_num = [map((int,input('Enter array: ').split()))]
+print(square_sum(input_num))
