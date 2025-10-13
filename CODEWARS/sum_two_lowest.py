7019eab342e4a8dd5c66d77ab37ae5041d1501eb
Naperality <napolytan@gmail.com>
2025-10-13 12:51:57 +0800

    add: sum of two lowest numbers

---
+++ CODEWARS/sum_two_lowest.py
@@ -0,0 +1,14 @@
+# Create a function that returns the sum of the two lowest positive numbers 
+# given an array of minimum 4 positive integers. No floats or non-positive integers 
+# will be passed.
+
+# For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+# [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+def sum_two_lowest(arr):
+    # arr.sort()
+    # return arr[0] + arr[1]
+    return sum(sorted(arr)[:2])
+
+print(sum_two_lowest(list(map(int, input('Enter array of numbers: ').split()))))
