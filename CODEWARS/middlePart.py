bdea89c02d3e989b2ce827c91f1d1edd567aa48a
Naperality <napolytan@gmail.com>
2025-10-14 12:15:22 +0800

    add: Middle Part String

--- CODEWARS/middlePart.py
+++ CODEWARS/middlePart.py
@@ -1,3 +1,10 @@
+# Find the Middle Part
+
+# Give a string, return the middle part 
+
+# If Odd, return the middle
+# Else Even, return the two get_middle
+
 def get_middle(s):
     # return s[int(len(s)/2)] if len(s) % 2 != 0 else s[int(len(s)/2)-1]+s[int(len(s)/2)]
     return s[(len(s)-1)//2:(len(s)+2)//2]
