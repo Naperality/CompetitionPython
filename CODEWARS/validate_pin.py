dcec96a55c88e3ab9bad56c144cff9cfe8d67ca4
Naperality <napolytan@gmail.com>
2025-11-10 14:30:39 +0800

    add: using regex

--- CODEWARS/validate_pin.py
+++ CODEWARS/validate_pin.py
@@ -8,8 +8,14 @@
 # "12345"  -->  false
 # "a234"   -->  false
 
+import re
+
 def validate_pin(pin):
     # return sum(1 if num.isdigit() else -10 for num in pin) in (4,6)
     return len(pin) in (4,6) and pin.isdigit()
 
+def validate_pin_re(pin):
+    return bool(re.fullmatch('\d{4}|\d{6}', pin))
+
 print(validate_pin(input('Enter Pin Number: ')))
+print(validate_pin_re(input('Enter Pin: ')))
