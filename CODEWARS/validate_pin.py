e8b12d14f58dd467c22f876775b8984117db7289
Naperality <napolytan@gmail.com>
2025-11-10 14:27:44 +0800

    add: Validation Pin

---
+++ CODEWARS/validate_pin.py
@@ -0,0 +1,14 @@
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot 
+# contain anything but exactly 4 digits or exactly 6 digits.
+
+# If the function is passed a valid PIN string, return true, else return false.
+
+# Examples (Input --> Output)
+# "1234"   -->  true
+# "12345"  -->  false
+# "a234"   -->  false
+
+def validate_pin(pin):
+    return sum(1 if num.isdigit() else -10 for num in pin) in (4,6)
+
+print(validate_pin(input('Enter Pin Number: ')))
