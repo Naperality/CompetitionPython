d78febfe4e07cf739113885e5f8b0ff35169588c
Naperality <napolytan@gmail.com>
2025-11-11 17:42:47 +0800

    add: Regex

---
+++ LESSONS/regex.py
@@ -0,0 +1,47 @@
+# Understanding regex in python and how to use them
+# When you have imported the re module, you can start using regular expressions:
+# Search the string to see if it starts with "The" and ends with "Spain":
+import re
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+# The regex functions:
+# The re module offers a set of functions that allows us to search a string for a match:
+# Function	Description
+# findall	Returns a list containing all matches
+# search	Returns a Match object if there is a match anywhere in the string
+# split	    Returns a list where the string has been split at each match
+# sub	    Replaces one or many matches with a string
+
+# Metacharacters:
+# []	A set of characters	"[a-m]"	
+# \	Signals a special sequence (can also be used to escape special characters)	"\d"	
+# .	Any character (except newline character)	"he..o"	
+# ^	Starts with	"^hello"	
+# $	Ends with	"planet$"	
+# *	Zero or more occurrences	"he.*o"	
+# +	One or more occurrences	"he.+o"	
+# ?	Zero or one occurrences	"he.?o"	
+# {}	Exactly the specified number of occurrences	"he.{2}o"	
+# |	Either or	"falls|stays"	
+# ()	Capture and group
+
+#Special Sequence:
+# \A	Returns a match if the specified characters are at the beginning of the string	"\AThe"	
+# \b	Returns a match where the specified characters are at the beginning 
+#       or at the end of a word
+#       (the "r" in the beginning is making sure that the string is being treated 
+#       as a "raw string")	r"\bain"  r"ain\b"	
+# \B	Returns a match where the specified characters are present, 
+#       but NOT at the beginning (or at the end) of a word
+#       (the "r" in the beginning is making sure that the string is being 
+#       treated as a "raw string")	r"\Bain" r"ain\B"	
+# \d	Returns a match where the string contains digits (numbers from 0-9)	"\d"	
+# \D	Returns a match where the string DOES NOT contain digits	"\D"	
+# \s	Returns a match where the string contains a white space character	"\s"	
+# \S	Returns a match where the string DOES NOT contain a white space character	"\S"	
+# \w	Returns a match where the string contains any word characters (characters from a to Z, 
+#       digits from 0-9, and the underscore _ character)	"\w"	
+# \W	Returns a match where the string DOES NOT contain any word characters	"\W"	
+# \Z	Returns a match if the specified characters are at the end of the string	"Spain\Z"
