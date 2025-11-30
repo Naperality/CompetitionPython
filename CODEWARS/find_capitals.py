69e9b625e2e8dde9da2a5f7c03c1bba786cb043e
Naperality <napolytan@gmail.com>
2025-11-30 10:07:19 +0800

    add: Find Capitals

---
+++ CODEWARS/find_capitals.py
@@ -0,0 +1,13 @@
+# Instructions
+# Write a function that takes a single non-empty string of only 
+# lowercase and uppercase ascii letters (word) as its argument, 
+# and returns an ordered list containing the indices of all capital 
+# (uppercase) letters in the string.
+
+# Example (Input --> Output)
+# "CodEWaRs" --> [0,3,4,6]
+
+def capitals_(word):
+    return [i for i, c in enumerate(word) if c.isupper()]
+
+print(capitals_(input('Enter Word: ')))
