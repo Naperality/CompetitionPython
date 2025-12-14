99c3d72191740cd98376703525ab874d5d28b134
Naperality <napolytan@gmail.com>
2025-12-14 23:59:34 +0800

    add: Interest Money

--- CODEWARS/interest_money.py
+++ CODEWARS/interest_money.py
@@ -34,6 +34,8 @@
 def calculate_years(principal, interest, tax, desired, n = 0):
     return n if principal>=desired else calculate_years(principal+((principal*interest)-(tax*interest)), interest, tax, desired, n+1)
 
+
+
 print(calculate_years(float(input('Enter Principal: '))
                       ,float(input('Enter Interest Rate: '))
                       ,float(input('Enter Tax Rate: '))
