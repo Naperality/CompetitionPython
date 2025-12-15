f5433f41d1009810ddf5cbed9ae39e2b75f99f0e
Naperality <napolytan@gmail.com>
2025-12-15 21:11:04 +0800

    add: Interest Rate and Desired

--- CODEWARS/count_num_squares.py
+++ CODEWARS/count_num_squares.py
@@ -7,3 +7,17 @@
 # Implement the function taking n and d as parameters and returning this count.
 
 # Examples:
+
+# n = 10, d = 1 
+# the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+# We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+# The function, when given n = 25 and d = 1 as argument, should return 11 since
+# the k*k that contain the digit 1 are:
+# 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+# So there are 11 digits 1 for the squares of numbers between 0 and 25.
+
+def nb_dig(n, d):
+    return sum(str(i**2).count(str(d)) for i in range(n+1))
+
+print(nb_dig(int(input('Enter Number of Times: ')), int(input('Enter find number: '))))
