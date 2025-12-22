cbc247d6fda575beed7ed8e043ab4523fb2654e6
Naperality <napolytan@gmail.com>
2025-12-22 09:46:37 +0800

    add: Iterative Solution

--- CODEWARS/absolute_sum_num.py
+++ CODEWARS/absolute_sum_num.py
@@ -11,4 +11,12 @@ def sum_digits(number):
     # return sum(int(i) for i in str(abs(number)))
     return sum(map(int,str(abs(number))))
 
+def iterative_solution(number):
+    res, number = 0, abs(number)    
+    while number > 0:
+        res += number%10
+        number //= 10
+    return res
+
 print(sum_digits(int(input('Enter Number: '))))
+print(sum_digits(iterative_solution(int(input('Enter Number: ')))))
