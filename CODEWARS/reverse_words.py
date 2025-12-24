f54d070a5858e78cd03fea6e507df0649f0f3fd6
Naperality <napolytan@gmail.com>
2025-12-24 10:28:34 +0800

    add: Reverse Whole Word Sentences

---
+++ CODEWARS/reverse_words.py
@@ -0,0 +1,15 @@
+# You need to write a function that reverses the words in a given string. 
+# Words are always separated by a single space.
+
+# As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+# Example (Input --> Output)
+
+# "Hello World" --> "World Hello"
+# "Hi There." --> "There. Hi"
+# Happy coding!
+
+def reverse(st):
+    return ' '.join(word[::-1] for word in st[::-1].strip().split())
+
+print(reverse(input('Enter Sentence: ')))
