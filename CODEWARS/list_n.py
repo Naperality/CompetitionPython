c63348721c3cb6e041b4241ef659a3d7331fd3eb
Naperality <napolytan@gmail.com>
2025-12-28 12:58:22 +0800

    add: List for N rnge

---
+++ CODEWARS/list_n.py
@@ -0,0 +1,10 @@
+# Create a function that accepts a list/array and a number n, and returns a list/array 
+# of the first n elements from the list/array.
+
+def take(arr,n):
+    return [arr[i] for i in range(min(n,len(arr)))]
+
+print(
+    take(list(map(int,input('Enter Array: ').split())),
+    int(input('Enter Number: ')))
+)
