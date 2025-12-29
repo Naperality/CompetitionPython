7e45ef27d896c9259dd146bd80df605e62d315f7
Naperality <napolytan@gmail.com>
2025-12-29 10:39:43 +0800

    add: Unique Order short version

--- CODEWARS/unique_order.py
+++ CODEWARS/unique_order.py
@@ -10,6 +10,20 @@
 # unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
 
 def unique_in_order(sequence):
-    return
+    if not sequence:
+        return []
+    res = []
+    for i in range(len(sequence)):
+        if i+1!=len(sequence) and sequence[i]!=sequence[i+1]:
+            res.append(sequence[i])
+    return res+[sequence[-1]]
+
+def other_solution(sequence):
+    res = []
+    for n in sequence:
+        if len(res)==0 or n!=res[-1]:
+            res.append(n)
+    return res
 
 print(unique_in_order(list(map(str,input('Enter List of Letters: ').split()))))
+print(other_solution(list(map(str,input('Enter Array: ').split()))))
