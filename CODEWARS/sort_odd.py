7dd4960f0920e0e956e4412f206a379024933e37
Naperality <napolytan@gmail.com>
2025-12-30 15:37:19 +0800

    add: Sort Odd Numbers in Array

---
+++ CODEWARS/sort_odd.py
@@ -0,0 +1,20 @@
+# Task
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order 
+# while leaving the even numbers at their original positions.
+
+# Examples
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+def sort_array(source_array):
+    odds = sorted([n for n in source_array if n%2!=0],reverse=True)
+    res = []
+    for i in range(len(source_array)):
+        if source_array[i]%2!=0:
+            res.append(odds.pop())
+        else:
+            res.append(source_array[i])
+    return res
+
+print(sort_array(list(map(int,input('Enter Array: ').split()))))
