faf3d08303b89cfdff9ad46328004ed34f3dfd3c
Naperality <napolytan@gmail.com>
2025-12-31 18:55:40 +0800

    add: Factorial Iteratively

--- CODEWARS/factorial_function.py
+++ CODEWARS/factorial_function.py
@@ -5,4 +5,12 @@ def factorial(n):
         return 1
     return factorial(n-1)*n
 
+def iteratively(n):
+    res = 1
+    while n>0:
+        res*=n
+        n-=1
+    return res
+
 print(factorial(int(input('Enter Number: '))))
+print(iteratively(int(input('Enter Number: '))))
