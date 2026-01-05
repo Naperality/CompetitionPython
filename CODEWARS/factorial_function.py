29cc751e4a30b2407473cc47e3f43ee517eb7a18
Naperality <napolytan@gmail.com>
2026-01-05 16:47:15 +0800

    add: Factorial with a Range

--- CODEWARS/factorial_function.py
+++ CODEWARS/factorial_function.py
@@ -12,5 +12,13 @@ def iteratively(n):
         n-=1
     return res
 
+def factorial_exception_error(n):
+    if 0 > n or n > 12:
+        raise ValueError('Invalid Input Range!')
+    if n == 0:
+        return 1
+    return n*factorial(n-1)
+
 print(factorial(int(input('Enter Number: '))))
 print(iteratively(int(input('Enter Number: '))))
+print(factorial_exception_error(int(input('Enter Number: '))))
