d504cee9994148ff963f4ebc0be624ed765331d0
Naperality <napolytan@gmail.com>
2026-03-15 22:30:10 +0800

    add: Problem 3

---
+++ REGIONAL/problem3.py
@@ -0,0 +1,30 @@
+# Problem
+# Write a function that checks whether a given password is acceptable.
+# A password is acceptable if:
+# Minimum length is 5
+# Contains at least one uppercase letter
+# Contains at least one digit
+# Does not contain space or '*'
+# First character must be an alphabet
+# Return 1 if valid, else 0.
+# Example
+# Input: Ab3$k
+# Output: 1
+# Input: 9Abc1
+# Output: 0
+
+def password_validator(string_):
+    if len(string_) < 5 or not string_[0].isalpha():
+        return 0
+    upper_, digit_, space_ = False, False, True 
+    for letter in string_:
+        if letter.isupper():
+            upper_ = True
+        if letter.isdigit():
+            digit_ = True
+        if letter in ('*',' '):
+            space_ = False
+
+    return upper_&digit_&space_
+
+print(password_validator(input('Enter Password: ')))
