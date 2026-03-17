c27703b5be0c15a5a3488bfd0001041a1debf592
Naperality <napolytan@gmail.com>
2026-03-17 23:59:36 +0800

    add:

---
+++ REGIONAL/problem5.py
@@ -0,0 +1,16 @@
+# Problem
+# Given two integers x and y, calculate:
+# (sum of numbers from 1 to y NOT divisible by x)
+# −
+# (sum of numbers from 1 to y divisible by x)
+# Example
+# Input:
+# x = 5
+# y = 15
+# Output:
+# 60
+
+def divisible_count(x,y):
+    return sum(n)
+
+print(divisible_count(int(input('Enter X: ')), int(input('Enter Y: '))))
