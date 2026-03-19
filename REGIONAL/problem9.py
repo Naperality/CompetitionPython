bffc33cbab385ebdf148a4a1e3c042ab0deef15b
Naperality <napolytan@gmail.com>
2026-03-19 23:02:36 +0800

    add: problem 9

---
+++ REGIONAL/problem9.py
@@ -0,0 +1,13 @@
+# Given an integer array, calculate: 
+# (sum of elements at prime indices) - (sum of elements at composite indices). 
+# Indexing starts at 0. Return 0 if array is empty.
+# Example
+# Input:
+# arr = [5,8,2,7,1,6]
+# Output:
+# -4
+
+def sum_prime_composite(array_):
+    pass
+
+print(sum_prime_composite(list(map(int, input("Enter Array Numbers: ").split()))))
