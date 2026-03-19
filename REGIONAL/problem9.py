a44e4d7371b2630e556e4ead9b0d0c4749092b86
Naperality <napolytan@gmail.com>
2026-03-19 23:10:30 +0800

    add: solution to problem 9

--- REGIONAL/problem9.py
+++ REGIONAL/problem9.py
@@ -7,7 +7,20 @@
 # Output:
 # -4
 
+def is_prime(num):
+    target = int(num**1/2)
+    while target!=0:
+        if num%target == 0: return False
+        target-=1
+    return True
+
 def sum_prime_composite(array_):
-    pass
+    if not array_: return 0
+    primes_t, composites_t = 0,0
+    for i,j in enumerate(array_):
+        if is_prime(i): primes_t+=j
+        else: composites_t+=j
+    return primes_t-composites_t
+
 
 print(sum_prime_composite(list(map(int, input("Enter Array Numbers: ").split()))))
