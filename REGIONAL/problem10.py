03ff61cc04a13f1476bd12d663a269b2b266cf13
Naperality <napolytan@gmail.com>
2026-03-20 21:27:27 +0800

    add: problem 10

---
+++ REGIONAL/problem10.py
@@ -0,0 +1,14 @@
+# Problem
+# Given two strings A and B, determine if B can be obtained by rotating A any number of times.
+# Return 1 if possible, else 0.
+# Example
+# Input:
+# A="abcd"
+# B="cdab"
+# Output:
+# 1
+
+def rotate_string(a,b):
+    pass
+
+print(rotate_string(input('Enter String A: '), input('Enter String B: ')))
