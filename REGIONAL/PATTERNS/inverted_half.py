905ee2f2f6096dd8404ca9d4d4d2345bb397ff1c
Naperality <napolytan@gmail.com>
2026-03-23 10:36:22 +0800

    add: inverted half

--- REGIONAL/PATTERNS/inverted_half.py
+++ REGIONAL/PATTERNS/inverted_half.py
@@ -7,8 +7,8 @@
 # where r = row
 
 def inverted_half(row):
-    for i in range(row):
-        for j in range(row+1-i):
+    for i in range(row): # or use for i in range(row,0,-1)
+        for j in range(row+1-i): #or use for j in range(0,i+1)
             print(j, end=' ')
         print()
     return ''
