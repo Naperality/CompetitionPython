c19ee1ea0693d76488a744c691833053a20d8bae
Naperality <napolytan@gmail.com>
2026-03-23 10:32:42 +0800

    add: inverted half

---
+++ REGIONAL/PATTERNS/inverted_half.py
@@ -0,0 +1,16 @@
+# Write a program that shows this pattern:
+# 0 1 2 3 4 5 
+# 0 1 2 3 4 
+# 0 1 2 3 
+# 0 1 2 
+# 0 1
+# where r = row
+
+def inverted_half(row):
+    for i in range(row+1):
+        for j in range(row+1):
+            print(j, end=' ')
+        print()
+    return ''
+
+print(inverted_half(int(input('Enter Number of Rows: '))))
