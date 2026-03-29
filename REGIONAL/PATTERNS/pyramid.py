41cd7ac5b0e487fe53f00e8ae961fcecd016e695
Naperality <napolytan@gmail.com>
2026-03-29 22:41:37 +0800

    udapte

--- REGIONAL/PATTERNS/pyramid.py
+++ REGIONAL/PATTERNS/pyramid.py
@@ -21,5 +21,13 @@ def inverted_pyramid(row):
         print(' '*(row-i)+'* '*i)
     return ''
 
+def pascal_pyramid(row):
+    line = [1]
+    for i in range(1,row+1):
+        print(' '*(row-i)+(' '.join(map(str,line))))
+        line = [1]+[line[j]+line[j+1] for j in range(len(line)-1)]+[1]
+    return ''
+
 print(pyramid_1(int(input('Enter Number of Rows: '))))
 print(inverted_pyramid(int(input('Enter Number of Rows: '))))
+print(pascal_pyramid(int(input('Enter Number of Rows: '))))
