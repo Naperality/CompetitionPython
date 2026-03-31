7280270eb813d364a9631f08699e700465faeefd
Naperality <napolytan@gmail.com>
2026-03-31 15:08:01 +0800

    add: solution to problem

--- REGIONAL/PATTERNS/letter_pattern.py
+++ REGIONAL/PATTERNS/letter_pattern.py
@@ -5,3 +5,33 @@
 # C       O   O   D   D   E
 # CCCCC   OOOOO   DDDDD   EEEEE
 
+def letter_pattern(spacing_=3):
+    for i in range(5):
+        # letter C
+        for j in range(5):
+            if i==0 or i==4 or j==0:
+                print('C', end='')
+            else:
+                print(' ', end='')
+        print(' '*spacing_, end='')
+
+        # letter O
+        for j in range(5):
+            if i==0 or i==4 or j==0 or j==4:
+                print('O', end='')
+            else:
+                print(' ', end='')
+        print(' '*spacing_, end='')
+
+        # letter D
+        for j in range(5):
+            if i==0 or i==4 or j==0 or j==4:
+                print('O', end='')
+            else:
+                print(' ', end='')
+        print(' '*spacing_, end='')
+
+        print()
+    return ''
+
+print(letter_pattern())
