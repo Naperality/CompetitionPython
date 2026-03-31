87b9d139868b01531dca651c10224de4361a6719
Naperality <napolytan@gmail.com>
2026-03-31 14:58:35 +0800

    add: Letter pattern

---
+++ REGIONAL/PATTERNS/letter_pattern.py
@@ -0,0 +1,7 @@
+# generate a program that shows the pattern CODE
+# CCCCC   OOOOO   DDDDD   EEEEE
+# C       O   O   D   D   E
+# C       O   O   D   D   EEE
+# C       O   O   D   D   E
+# CCCCC   OOOOO   DDDDD   EEEEE
+
