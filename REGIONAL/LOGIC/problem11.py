2e158da9e1610485e4e16b4ebbedb74d4511cc45
Naperality <napolytan@gmail.com>
2026-04-02 19:14:15 +0800

    add

--- REGIONAL/LOGIC/problem11.py
+++ REGIONAL/LOGIC/problem11.py
@@ -1,6 +1,7 @@
-# Make a program to calculate the Area of the Rectangle
-
-def area_(side):
-    pass
+# Make a program to calculate the Area of the Regular Pentagon
+import math
+def area_(side_ = 1):
+    res = ((math.sqrt(5*(5+(2*math.sqrt(5))))/4))*side_**2
+    return f'Area is: {res}'
 
 print(area_(float(input('Enter one side: '))))
