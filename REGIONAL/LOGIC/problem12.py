0533825cb9db3fcd1b83a6589a917babe4825c49
Naperality <napolytan@gmail.com>
2026-04-03 14:24:28 +0800

    ad

---
+++ REGIONAL/LOGIC/problem12.py
@@ -0,0 +1,16 @@
+# Problem
+# Given two strings A and B, determine if B can be obtained by any combination A any number of times.
+# Return 1 if possible, else 0.
+# Example
+# Input:
+# A="abcd"
+# B="cdab"
+# Output:
+# 1
+
+def sol_(a,b):
+    if len(a) != len(b): return 0
+    if b in (a+a): return 1
+    return 0
+
+print(sol(input('Enter String A: '), input('Enter String B: ')))
