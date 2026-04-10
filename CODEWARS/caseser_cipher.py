5ba3a4a4861043eb7fad7c1e3218a335add4c13d
Naperality <napolytan@gmail.com>
2026-04-10 19:59:25 +0800

    add: caeser cipher

---
+++ CODEWARS/caseser_cipher.py
@@ -0,0 +1,8 @@
+# Make a program Caeser's Cipher
+# where it accepts an string and a shift key for the letter to shift
+# Make also a deciphering logic for it too in one function
+
+def caeser_cipher(string_, shift_, mode_):
+    pass
+
+print(caeser_cipher(input('Enter a string: '), int(input('Enter a shift number: ')), int(input('Enter mode (0-cipher,1-decipher): '))))
