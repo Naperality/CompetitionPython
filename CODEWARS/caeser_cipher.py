fc223d5dbce4a75cc907255a4b074427e4044a8d
Naperality <napolytan@gmail.com>
2026-04-10 20:25:03 +0800

    add: Caeser Cipher

--- CODEWARS/caeser_cipher.py
+++ CODEWARS/caeser_cipher.py
@@ -3,6 +3,7 @@
 # Make also a deciphering logic for it too in one function
 
 def caeser_cipher(string_, shift_, mode_):
-    pass
+    if mode_==1: shift_*=-1
+    return ''.join(chr((ord(l)-(b:=ord('a') if l.islower() else ord('A'))+shift_)%26+b) if l.isalpha() else l for l in string_)
 
 print(caeser_cipher(input('Enter a string: '), int(input('Enter a shift number: ')), int(input('Enter mode (0-cipher,1-decipher): '))))
