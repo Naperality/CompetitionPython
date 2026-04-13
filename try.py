57f45787139607a2f1446000b5dbb18a494c7692
Naperality <napolytan@gmail.com>
2026-04-13 21:11:28 +0800

    .

--- try.py
+++ try.py
@@ -3,5 +3,9 @@ print(str_.count('o'))
 print(str_.translate(str.maketrans({'o':'','0':''})))
 print(str_.partition(':'))
 
+
+
+
+
 str_1 = 'AAAAAAA'
 print([str_1[i:i+3] for i in range(0,len(str_1)-3+1,3) if str_1[i:i+3].startswith('A')])
