6c5dfc93b1707f742fe062b0e716735b3c8d21ea
Naperality <napolytan@gmail.com>
2026-05-09 21:59:00 +0800

    update: Substrings problem

--- LESSONS/substrings.py
+++ LESSONS/substrings.py
@@ -12,3 +12,21 @@ print(max_substrings_length(
     list(map(int, input('Enter list of Numbers: ').split())),
     int(input('Enter K(number) max size of substring: '))
 ))
+
+# Make another program that calculates the number of substrings with
+# max sum of limit K 
+
+def max_sum_substring(array_,k_):
+    count_, total, left= 0, 0, 0
+    for right in range(len(array_)):
+        total += array_[right]
+        while total > k_ and left <= right:
+            total -= array_[left]
+            left += 1
+        count_ += (right-left+1)
+    return count_
+
+print(max_sum_substring(
+    list(map(int, input('Enter array of numbers: ').split())),
+    int(input('Enter K Sum Limit: '))
+))
